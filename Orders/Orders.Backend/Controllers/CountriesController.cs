383ee1a0e62a6ad8dd2e13b10c2494d529952413
Juan Carlos Ocha <juan.ochoa@sonorasoft.com>
2024-05-10 08:40:10 -0700

    Add basic countries api controller

--- Orders/Orders.Backend/Controllers/CountriesController.cs
+++ Orders/Orders.Backend/Controllers/CountriesController.cs
@@ -1,18 +1,31 @@
 ﻿namespace Orders.Backend.Controllers
 {
     using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
     using Orders.Backend.Data;
     using Orders.Shared.Entities;
 
     [ApiController]
     [Route("api/[controller]")]
-    public class CountriesController : ControllerBase
+    public class CountriesController(DataContext context) : ControllerBase
     {
-        private readonly DataContext _context;
+        private readonly DataContext _context = context;
 
-        public CountriesController(DataContext context)
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
         {
-            _context = context;
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(Guid id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
         }
 
         [HttpPost]
@@ -22,6 +35,30 @@
             await _context.SaveChangesAsync();
 
             return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteAsync(Guid id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
         }
     }
 }
--- Orders/Orders.Backend/Program.cs
+++ Orders/Orders.Backend/Program.cs
@@ -3,16 +3,18 @@ using Orders.Backend.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<DataContext>(x => x.UseSqlServer("name=LocalConnection"));
 
 var app = builder.Build();
+
+app.UseCors(x =>
+    x.AllowAnyMethod()
+    .AllowAnyHeader()
+    .SetIsOriginAllowed(origin => true)
+    .AllowCredentials());
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
